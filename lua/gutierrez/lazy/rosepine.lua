bfc9b779510c62c5a2c49032938fc5fb5fd469a2
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2025-11-03 09:45:00 -0300

    remove italic from rosepine

--- lua/gutierrez/lazy/rosepine.lua
+++ lua/gutierrez/lazy/rosepine.lua
@@ -1,28 +1,27 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = true,
-		config = function()
-			require('rose-pine').setup({
-				disable_background = true,
-				styles = {
-					bold = false,
-					italic = true,
-					transparncy = true
-				},
-			})
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = true,
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = {
+                    bold = false,
+                    italic = false,
+                    transparncy = true,
+                },
+            })
 
-			ColorMyPencils()
-		end
-	}
+            ColorMyPencils()
+        end,
+    },
 }
